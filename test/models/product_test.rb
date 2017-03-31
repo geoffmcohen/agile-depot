6531c85a60b8cb2d43ba7198251053ccc7a7e567
Geoff Cohen <geoffmcohen@gmail.com>
2017-03-31 12:47:51 -0400

    Adding even more model tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-	
+  fixtures :products
+  
   test "product attributes must not be empty" do
   	  product = Product.new
   	  assert product.invalid?
@@ -41,5 +42,12 @@ class ProductTest < ActiveSupport::TestCase
   	  bad.each do |name|
   	  	  assert new_product(name).invalid?, "#{name} shouldn't be valid"
   	  end
+  end
+  
+  #Test using data from the fixtures
+  test "product is not valid without a unique title" do
+  	  product = Product.new(title: products(:ruby).title, description: "yyy", price: 1, image_url: "fred.gif")
+  	  assert product.invalid?
+  	  assert_equal ["has already been taken"], product.errors[:title]
   end
 end
