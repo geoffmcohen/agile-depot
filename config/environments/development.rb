eca44e151df7129e16dc9a869e2ec03978637693
Geoff Cohen <geoffmcohen@gmail.com>
2017-04-02 14:56:07 -0400

    Adding a products channel to update the store when a product is updated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  # Allow other hosts to access the channels
+  config.action_cable.disable_request_forgery_protection = true
 end
